a8ca3d4186c328cc697bd8f3547832cabf764c0a
Libor Váchal <libor.vachal@gmail.com>
2016-04-17 23:35:47 +0200

    * modify disk info script

--- WinRemoteAdministration/App_Data/PScripts/showDiskSpace.ps1
+++ WinRemoteAdministration/App_Data/PScripts/showDiskSpace.ps1
@@ -1,10 +1,6 @@
-﻿param (
-    [string]$Disk
-)
-
-Try {
-    $DiskParam = "DeviceID='" + $Disk +":'"
-    Get-WmiObject Win32_LogicalDisk -ComputerName $env:COMPUTERNAME -Filter $DiskParam
-} Catch {
-    return -1
-}
+﻿Get-PSDrive -PSProvider Filesystem |
+    Where-Object {$_.Used -ne 0 -and $_.Free -ne $null} |
+    Select-Object -property @{Name="Name";Expression={$_.name}},
+        @{Name="Free";Expression={$_.free / 1GB}},
+        @{Name="Used";Expression={$_.used / 1GB}},
+        @{Name="Total";Expression={($_.used + $_.free) / 1GB}}
